0e88fb0f8cc2185c24f894e7df8cc7d9d401bd35
VQuine <01010110.v@gmail.com>
2020-06-27 21:44:19 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,24 @@ class App < Sinatra::Base
     end
 
     post '/team' do
-      @team = params[:team]
+       @team_name = params[:team][:name]
+       @team_motto = params[:team][:motto]
+
+       @member_1_name = params[:team][:members][0][:name]
+       @member_1_power = params[:team][:members][0][:power]
+       @member_1_bio = params[:team][:members][0][:bio]
+
+       @member_2_name = params[:team][:members][1][:name]
+       @member_2_power = params[:team][:members][1][:power]
+       @member_2_bio = params[:team][:members][1][:bio]
+
+       @member_3_name = params[:team][:members][2][:name]
+       @member_3_power = params[:team][:members][2][:power]
+       @member_3_bio = params[:team][:members][2][:bio]
+     
+
+      
+
         erb :team
     end
 
